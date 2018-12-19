a858b251efd4f325444e5c902f4048f60d1dcc48
Caleb Campbell <38718983+MrChido@users.noreply.github.com>
2018-12-19 13:59:03 -0600

    The beginning of another game aid.
    A straight up simple design used to employ the core feature of this app. THis particular upload is to facilitate geting the app ready to have mobile viewing.

---
+++ warbandJS.js
@@ -0,0 +1,14 @@
+const nuVals = document.getElementById("nuVal");
+
+
+nuVals.addEventListener('click', ()=>{
+Vanguard1.value="0/0:0";
+Vanguard2.value="0/0:0";
+Vanguard3.value="0/0:0";
+Flank1.value="0/0:0";
+Flank2.value="0/0:0";
+Rear1.value="0/0:0";
+Rear2.value="0/0:0";
+Rear3.value="0/0:0";
+wLeader.value="0/0:0";
+});
