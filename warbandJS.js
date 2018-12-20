23721b665f6efc386f81533ce0311af480185d47
Caleb Campbell <38718983+MrChido@users.noreply.github.com>
2018-12-20 14:23:28 -0600

    color tweaks

--- warbandJS.js
+++ warbandJS.js
@@ -2,13 +2,13 @@ const nuVals = document.getElementById("nuVal");
 
 
 nuVals.addEventListener('click', ()=>{
-Vanguard1.value="0/0:0";
-Vanguard2.value="0/0:0";
-Vanguard3.value="0/0:0";
-Flank1.value="0/0:0";
-Flank2.value="0/0:0";
-Rear1.value="0/0:0";
-Rear2.value="0/0:0";
-Rear3.value="0/0:0";
-wLeader.value="0/0:0";
+Vanguard1.value="";
+Vanguard2.value="";
+Vanguard3.value="";
+Flank1.value="";
+Flank2.value="";
+Rear1.value="";
+Rear2.value="";
+Rear3.value="";
+wLeader.value="";
 });
